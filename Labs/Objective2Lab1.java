d632d2eafabaa99c9b049b976bd93276a89e4d15
Chelsey S <chelseyann2119@gmail.com>
2020-12-05 14:34:27 -0700

    complete objective2lab1

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,8 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    //Declare and initial variable hello
+    String hello = "Hello World";
+    //Print hello
+    System.out.println(hello);
+  }
+}
