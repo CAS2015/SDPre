a39569f798849b0bd6c93b85bf5e97e1b3689814
Chelsey S <chelseyann2119@gmail.com>
2020-12-05 14:44:01 -0700

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    //declaring and initializing name, age, state variables
+    String name = "Chelsey Sassine";
+    int age = 32;
+    String state = "Nebraska";
+    //printing personal information
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
