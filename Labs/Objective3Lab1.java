1c27e8b882e23804ad871506ca6e6d6923a04450
Chelsey S <chelseyann2119@gmail.com>
2020-12-07 14:14:59 -0700

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,13 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    //initializing variables
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+    //formula to initalize average variable
+    average = (num1 + num2 + num3)/3;
+    //print results of average
+    System.out.println(average);
+  }
+}
