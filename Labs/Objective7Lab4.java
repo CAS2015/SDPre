def42711f16160513f70fd50fcbdc1c04b66fccf
Chelsey S <chelseyann2119@gmail.com>
2020-12-11 13:24:47 -0700

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+  public static void main(String[] args)  {
+    int counter = 1, totalSum = 0;
+
+    while(counter <= 20) {
+      totalSum += counter;
+      counter ++;
+    }
+    System.out.println(totalSum);
+  }
+}
