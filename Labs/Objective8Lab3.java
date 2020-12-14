cac3d4eb3b090fc7ea4cfd0bc77e370c0563932d
Chelsey S <chelseyann2119@gmail.com>
2020-12-14 12:06:15 -0700

    complete objective8lab3

--- Labs/Objective8Lab3.java
+++ Labs/Objective8Lab3.java
@@ -1,8 +1,10 @@
 public class Objective8Lab3 {
   public static void main(String[] args) {
     int counter = 1;
+
     for(int i = 1; i <= 20; i++) {
       counter = i % 2;
+
       switch (counter) {
         case 0:
           System.out.println(i + " is even");
